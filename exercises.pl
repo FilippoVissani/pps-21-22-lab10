2109b0aeaa21421aa84bd0f107606f4b13765848
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-11-06 12:05:52 +0100

    Added exercise 1.4

--- exercises.pl
+++ exercises.pl
@@ -1,15 +1,22 @@
 % search(Elem, List)
-search(H, [H|_]).
-search(X, [_|T]) :- search(X, T).
+search(E, [E|_]).
+search(E, [_|T]) :- search(E, T).
 
 % search2(Elem , List)
 % looks for two consecutive occurrences of Elem
-search2(X, [X, X|_]).
-search2(X, [_|T]) :- search2(X, T).
+search2(E, [E, E|_]).
+search2(E, [_|T]) :- search2(E, T).
 
 % search_two(Elem, List)
 % looks for two occurrences of Elem with any element in between!
 % search_two(a,[b,c,a,a,d,e]). → no
 % search_two(a,[b,c,a,d,a,d,e]). → yes
-search_two(X, [X, _, X|_]).
-search_two(X, [_|T]) :- search_two(X, T).
+search_two(E, [E, _, E|_]).
+search_two(E, [_|T]) :- search_two(E, T).
+
+% search_anytwo(Elem, List)
+% looks for any Elem that occurs two times, anywhere
+% search_anytwo(a,[b,c,a,a,d,e]). → yes
+% search_anytwo(a,[b,c,a,d,e,a,d,e]). → yes
+search_anytwo(E, [E|T]) :- search(E, T).
+search_anytwo(E, [_|T]) :- search_anytwo(E, T).
