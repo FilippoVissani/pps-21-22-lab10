d9d7c4d2a47716b32959a9b9f89de3cddcaf12c1
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-11-06 12:08:43 +0100

    Added exercises numbers

--- exercises.pl
+++ exercises.pl
@@ -1,12 +1,15 @@
+% exercise 1.1
 % search(Elem, List)
 search(E, [E|_]).
 search(E, [_|T]) :- search(E, T).
 
+% exercise 1.2
 % search2(Elem , List)
 % looks for two consecutive occurrences of Elem
 search2(E, [E, E|_]).
 search2(E, [_|T]) :- search2(E, T).
 
+% exercise 1.3
 % search_two(Elem, List)
 % looks for two occurrences of Elem with any element in between!
 % search_two(a,[b,c,a,a,d,e]). → no
@@ -14,9 +17,12 @@ search2(E, [_|T]) :- search2(E, T).
 search_two(E, [E, _, E|_]).
 search_two(E, [_|T]) :- search_two(E, T).
 
+% exercise 1.4
 % search_anytwo(Elem, List)
 % looks for any Elem that occurs two times, anywhere
 % search_anytwo(a,[b,c,a,a,d,e]). → yes
 % search_anytwo(a,[b,c,a,d,e,a,d,e]). → yes
 search_anytwo(E, [E|T]) :- search(E, T).
 search_anytwo(E, [_|T]) :- search_anytwo(E, T).
+
+% exercise 2.1
